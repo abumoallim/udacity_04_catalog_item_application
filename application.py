c413ad1d6d5b11ad536199dc567439597c38f810
AbubakkerLinuxHome <abumoallim16@gmail.com>
2018-09-17 12:12:40 +0400

    Initial commit

---
+++ application.py
@@ -0,0 +1,415 @@
+from flask import Flask, session, render_template
+from flask import request, redirect, jsonify, url_for, make_response
+from database_setup import db, Catalog, CatalogItem, User
+from flask_login import LoginManager, current_user
+from flask_login import login_required, login_user, logout_user
+from flask import g
+from flask import flash
+from requests_oauthlib import OAuth2Session
+import os
+import datetime
+import jwt
+from flask_migrate import Migrate
+import json
+from functools import wraps
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///itemcatalog.db'
+db.init_app(app)
+Migrate(app, db, render_as_batch=True)
+app.secret_key = "H7DPWNMQOVKSYUAmufrySUYYUS"
+
+# oauthlib - oauth2 must utilize https workaround
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+
+# initiate login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class Auth:
+    """Google Project Credentials"""
+    CLIENT_ID = ('747601484914-urhn3gji4cjahmgqllcnajnbs44oi3p2'
+                 '.apps.googleusercontent.com')
+    CLIENT_SECRET = 'Byc6YxroZYbPN4kRNOhtSXgZ'
+    REDIRECT_URI = 'http://localhost:5000/gCallback'
+    AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+    TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+    USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
+    SCOPE = ['profile', 'email']
+
+
+# Loads Users
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+# This will call before each to see if current_user is not None
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+# User Registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'],
+                request.form['password'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+
+# User Normal login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        google = get_google_auth()
+        auth_url, state = google.authorization_url(
+            Auth.AUTH_URI, access_type='offline')
+        session['oauth_state'] = state
+        return render_template('login.html', auth_url=auth_url)
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(
+        username=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+# User Logout
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+# User Google Login
+def get_google_auth(state=None, token=None):
+    if token:
+        return OAuth2Session(Auth.CLIENT_ID, token=token)
+    if state:
+        return OAuth2Session(
+            Auth.CLIENT_ID,
+            state=state,
+            redirect_uri=Auth.REDIRECT_URI)
+    oauth = OAuth2Session(
+        Auth.CLIENT_ID,
+        redirect_uri=Auth.REDIRECT_URI,
+        scope=Auth.SCOPE)
+    return oauth
+
+
+# Google sign in callback
+@app.route('/gCallback')
+def callback():
+    if current_user is not None and current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if 'error' in request.args:
+        if request.args.get('error') == 'access_denied':
+            return 'You denied access.'
+        return 'Error encountered.'
+    if 'code' not in request.args and 'state' not in request.args:
+        return redirect(url_for('login'))
+    else:
+        google = get_google_auth(state=session['oauth_state'])
+        try:
+            token = google.fetch_token(
+                Auth.TOKEN_URI,
+                client_secret=Auth.CLIENT_SECRET,
+                authorization_response=request.url)
+        except:
+            return 'HTTPError occurred.'
+        google = get_google_auth(token=token)
+        resp = google.get(Auth.USER_INFO)
+        if resp.status_code == 200:
+            user_data = resp.json()
+            email = user_data['email']
+            user = User.query.filter_by(email=email).first()
+            if user is None:
+                user = User()
+                user.email = email
+            user.username = user_data['email']
+            print(token)
+            user.social_auth_token = json.dumps(token)
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            return redirect(url_for('index'))
+    return 'Could not fetch your information.'
+
+
+# Show all catalogs
+@app.route('/')
+@app.route('/catalog/')
+def index():
+    catalogs = Catalog.query.limit(10)
+    catalogItems = CatalogItem.query.order_by(CatalogItem.id.desc()).all()
+    return render_template('index.html',
+                           catalogs=catalogs,
+                           catalogItems=catalogItems)
+
+
+# Create a new Catalog
+@app.route('/catalog/new/', methods=['GET', 'POST'])
+@login_required
+def addCatalog():
+    if request.method == 'POST':
+        newCatalog = Catalog(name=request.form['catalog'])
+        db.session.add(newCatalog)
+        db.session.commit()
+        flash('Catalog Added Succesfully')
+        return redirect(url_for('index'))
+    else:
+        return render_template('add_catalog.html', method="ADD")
+
+
+# Updating catalog
+@app.route('/catalog/new/<int:catalog_id>/edit', methods=['GET', 'POST'])
+@login_required
+def updateCatalog(catalog_id):
+    catalog = Catalog.query.filter_by(id=catalog_id).first()
+    if request.method == 'POST':
+        if request.form['catalog']:
+            catalog.name = request.form['catalog']
+            db.session.add(catalog)
+            db.session.commit()
+            flash('Catalog Updated Succesfully')
+            return redirect(url_for('index'))
+    else:
+        return render_template('add_catalog.html',
+                               catalog=catalog,
+                               method="UPDATE")
+
+
+# Deleting catalog
+@app.route('/catalog/<int:catalog_id>/delete/', methods=['GET', 'POST'])
+@login_required
+def deleteCatalog(catalog_id):
+    catalog = Catalog.query.filter_by(id=catalog_id).first()
+    catalogItems = CatalogItem.query.filter_by(catalog_id=catalog_id).all()
+    if request.method == "POST":
+        for items in catalogItems:
+            db.session.delete(items)
+        db.session.delete(catalog)
+        db.session.commit()
+        flash('Catalog Deleted Succesfully')
+
+        return redirect(url_for('index'))
+    else:
+                return render_template('delete.html',
+                                       catalog=catalog,
+                                       delete_type="catalog")
+
+
+# Showing items for particular Catalog
+@app.route('/catalog/<int:catalog_id>/items/')
+def catalogDetails(catalog_id):
+    catalog = Catalog.query.filter_by(id=catalog_id).first()
+    catalogItems = CatalogItem.query.filter_by(catalog_id=catalog_id).all()
+    return render_template('catalog_item.html',
+                           catalog=catalog,
+                           catalogItems=catalogItems)
+
+
+# Creating new item in catalog
+@app.route('/catalog/<int:catalog_id>/item/new', methods=['GET', 'POST'])
+@login_required
+def addItem(catalog_id):
+    if request.method == 'POST':
+        newItem = CatalogItem(name=request.form['item_name'],
+                              description=request.form['item_description'],
+                              catalog_id=request.form['catalog_select'])
+        db.session.add(newItem)
+        db.session.commit()
+        flash('Item Added Succesfully')
+
+        return redirect(url_for('catalogDetails',
+                        catalog_id=newItem.catalog_id))
+
+    else:
+        catalogs = Catalog.query.all()
+        catalog = Catalog.query.filter_by(id=catalog_id).first()
+        return render_template('add_item.html',
+                               catalogs=catalogs,
+                               catalog=catalog,
+                               method="ADD")
+
+
+# Updating item
+@app.route('/catalog/<int:catalog_id>/item/<int:item_id>/edit/',
+           methods=['GET', 'POST'])
+@login_required
+def updateItem(catalog_id, item_id):
+    catalogItem = CatalogItem.query.filter_by(id=item_id).first()
+    if request.method == 'POST':
+        if request.form['item_name']:
+            catalogItem.name = request.form['item_name']
+        if request.form['item_description']:
+            catalogItem.description = request.form['item_description']
+        if request.form['catalog_select']:
+            catalogItem.catalog_id = request.form['catalog_select']
+        db.session.add(catalogItem)
+        db.session.commit()
+        flash('Item Updated Succesfully')
+
+        return redirect(url_for('catalogDetails',
+                        catalog_id=catalogItem.catalog_id))
+
+    else:
+        catalogs = Catalog.query.all()
+        catalog = Catalog.query.filter_by(id=catalog_id).first()
+        return render_template('add_item.html',
+                               catalogs=catalogs,
+                               catalog=catalog,
+                               catalogItem=catalogItem,
+                               method="UPDATE")
+
+
+# Deleting item
+@app.route('/catalog/<int:catalog_id>/item/<int:item_id>/delete/',
+           methods=['GET', 'POST'])
+@login_required
+def deleteItem(catalog_id, item_id):
+    catalogItem = CatalogItem.query.filter_by(id=item_id).first()
+    if request.method == 'POST':
+        db.session.delete(catalogItem)
+        db.session.commit()
+        flash('Item Deleted Succesfully')
+
+        return redirect(url_for('catalogDetails',
+                        catalog_id=catalogItem.catalog_id))
+    else:
+        return render_template('delete.html',
+                               catalog_id=catalog_id,
+                               catalogItem=catalogItem,
+                               delete_type="item")
+
+
+# Showing item details
+@app.route('/catalog/<int:catalog_id>/item/<item_id>/')
+@login_required
+def itemDetails(catalog_id, item_id):
+    catalog = Catalog.query.filter_by(id=catalog_id).first()
+    catalogItem = CatalogItem.query.filter_by(id=item_id).first()
+    return render_template('item_display.html',
+                           catalogItem=catalogItem,
+                           catalog=catalog)
+
+
+# APIs
+# Custom Decorator
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'authorization_token' in request.headers:
+            token = request.headers['authorization_token']
+        if not token:
+            return jsonify({'message': 'Login Required!'}), 401
+        try:
+            data = jwt.decode(token, app.secret_key)
+            current_user = User.query.filter_by(id=data['id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+# Login
+@app.route('/api/login', methods=['POST'])
+def restLogin():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    if not username or not password:
+        return make_response('Params not found', 401)
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return make_response('Could not verify', 401)
+
+    registered_user = User.query.filter_by(
+        username=username, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('login'))
+    token = jwt.encode({'id': user.id}, app.secret_key)
+    return jsonify({'token': token.decode('UTF-8')})
+
+
+# Get Catalogs
+@app.route('/api/catalog/')
+@token_required
+def catalogs_json(current_user):
+    catalogs = Catalog.query.all()
+    return jsonify([c.serialize for c in catalogs])
+
+
+# Create a new Catalog
+@app.route('/api/catalog/new/', methods=['POST'])
+@token_required
+def addCatalogAPI(current_user):
+        catalog_name = request.args.get('catalog_name')
+        newCatalog = Catalog(name=catalog_name)
+        db.session.add(newCatalog)
+        db.session.commit()
+        return jsonify(id=newCatalog.id, name=newCatalog.name)
+
+
+# Updating catalog
+@app.route('/api/catalog/edit/', methods=['POST'])
+@token_required
+def updateCatalogAPI(current_user):
+    catalog_id = request.args.get('catalog_id')
+    updateCatalog = Catalog.query.filter_by(id=catalog_id).first()
+    if request.method == 'POST':
+        if request.args.get('catalog_name'):
+            updateCatalog.name = request.args.get('catalog_name')
+            db.session.add(updateCatalog)
+            db.session.commit()
+            return jsonify(id=updateCatalog.id, name=updateCatalog.name)
+
+
+# Deleting catalog
+@app.route('/api/catalog/delete/', methods=['POST'])
+@token_required
+def deleteCatalogAPI(current_user):
+    catalog_id = request.args.get('catalog_id')
+    catalog = Catalog.query.filter_by(id=catalog_id).first()
+    catalogItems = CatalogItem.query.filter_by(catalog_id=catalog_id).all()
+    if request.method == "POST":
+        for items in catalogItems:
+            db.session.delete(items)
+        db.session.delete(catalog)
+        db.session.commit()
+        return jsonify('Catalog Deleted Succesfully')
+
+
+# Showing items for particular Catalog
+@app.route('/api/catalog/items/', methods=['GET', 'POST'])
+@token_required
+def catalogDetailsAPI(current_user):
+    catalog_id = request.args.get('catalog_id')
+    catalogItems = CatalogItem.query.filter_by(catalog_id=catalog_id).all()
+    return jsonify([c.serialize for c in catalogItems])
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ database_setup.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import datetime
+import random
+import string
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+# User Model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    social_auth_token = db.Column(
+        'social_auth_token', db.String(50), default="")
+    token = db.Column('token', db.String(50), default="")
+    registered_on = db.Column(
+        'registered_on', db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return (self.id)
+
+    def __repr__(self):
+        return (self.username)
+
+
+# Catalog Model
+class Catalog(db.Model):
+    __tablename__ = "catalog"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+# Catalog Item
+class CatalogItem(db.Model):
+    __tablename__ = "catalog_item"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    catalog_id = db.Column(db.Integer, db.ForeignKey('catalog.id'))
+    catalog = db.relationship(Catalog)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'catalog_id': self.catalog_id,
+        }
